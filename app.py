51f9991fbb9df696ef3a777525367593dfc0825a
Ajay Singh <42219497+maverick27031989@users.noreply.github.com>
2020-06-25 14:49:15 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from ProductionWorker import ProductionWorker
+import pyodbc
 
 name = input("Enter employee name: ")
 emp_number = input("Enter employee number: ")
@@ -19,6 +20,43 @@ print("Employee number: " + productionWorker.get_emp_number())
 shift_number = productionWorker.get_shift_number()
 if shift_number == 1:
     print("Shift: Day")
+    shiftType = "Day Shift"
 else:
     print("Shift: Night")
+    shiftType = "Night Shift"
 print("Hourly per rate: " + productionWorker.get_hourly_pay_rate())
+
+
+def read(conn):
+    print("Reading function")
+    cur = conn.cursor()
+    cur.execute("select * from ProductionWorker")
+    for row in cur:
+        print(f'row = {row}')
+    print()
+
+
+def create(conn):
+    print("Create Function")
+    cur = conn.cursor()
+    cur.execute(
+        'insert into ProductionWorker(name, emp_number, shift_type, hourly_pay_rate) values(?,?,?,?);',
+        (productionWorker.get_name(),
+         productionWorker.get_emp_number(),
+         shiftType,
+         productionWorker.get_hourly_pay_rate())
+    )
+    conn.commit()
+    read(conn)
+
+
+conn = pyodbc.connect(
+    "Driver={SQL Server Native Client 11.0};"
+    "Server=DESKTOP-EA06L8B;"
+    "Database=call;"
+    "Trusted_Connection=yes;"
+)
+
+read(conn)
+create(conn)
+
